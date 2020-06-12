008da6623e8a38c17a73bde433a3b15ae9bb3727
Krushna Kanchani <krushna.kanchani@neudesic.com>
2020-06-12 17:51:50 +0530

    Fluent Builder changes

---
+++ Builder/FluentbuilderSqlParameter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace Builder
+{
+     public class FluentbuilderSqlParameter
+    {
+        public ICollection<SqlParameter> parameters;
+        public FluentbuilderSqlParameter(ICollection<SqlParameter> sqlParameters)
+        {
+            this.parameters = sqlParameters;
+        }
+
+        public FluentbuilderSqlParameter Add(string name, object value)
+        {
+            parameters.Add(new SqlParameter(name, value));
+            return this;
+        }
+    }
+}
--- Builder/Program.cs
+++ Builder/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Runtime.InteropServices.ComTypes;
 
 namespace Builder
 {
@@ -6,7 +10,68 @@ namespace Builder
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            FacadePersonBuilder.TestBuilder();
+
+            ICollection<SqlParameter> collection = new FluentbuilderSqlParameter(new List<SqlParameter>())
+                                                        .Add("test", "test")
+                                                        .Add("test", "test")
+                                                        .parameters;
+
+
+            var person = Person.New.Called("krushna").WorkAsA("Soft").Build();
+            Console.WriteLine(person);
+            Console.ReadKey();
+        }
+
+
+
+        public class Person
+        {
+            public string Name { get; set; }
+
+            public string Position { get; set; }
+
+            public static Builder New => new Builder();
+
+            public class Builder: PerosnJobBuilder<Builder>
+            {
+
+            }
+
+            public override string ToString()
+            {
+                return Name;
+            }
+        }
+
+        public abstract class PersonBuilder
+        {
+            protected Person person = new Person();
+
+            public Person Build() => person; 
+
+        }
+
+        public class PerosnInfoBuilder<Self> : PersonBuilder 
+            where Self : PerosnInfoBuilder<Self> 
+        {
+
+            public Self Called(string name)
+            {
+                person.Name = name;
+                return (Self)this;
+            }
+        }
+
+        public class PerosnJobBuilder<Self>: PerosnInfoBuilder<PerosnJobBuilder<Self>>
+            where Self: PerosnJobBuilder<Self>
+             
+        {
+            public Self WorkAsA(string postion)
+            {
+                person.Position = postion;
+                return (Self) this;
+            }
         }
     }
 }
